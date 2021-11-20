bd9d47898de6da75747d9b66936871e59e79ce4f
Gwendal <gwendou95@hotmail.fr>
2021-11-20 18:57:09 +0100

    Static influencers to develop

---
+++ src/assets/influencers.js
@@ -0,0 +1,34 @@
+export const influencers = [
+    {
+        id: 1,
+        name: 'Gwendal Siwiorek',
+        job: 'Software Engineer',
+        description: 'Trust me I\'m a wonderful engineer',
+        avatar:
+            'https://scontent-cdg2-1.xx.fbcdn.net/v/t1.18169-9/24852611_1499818266739132_5187376035313257790_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=0hw6NdNgJrMAX8eJmDh&_nc_ht=scontent-cdg2-1.xx&oh=9fc5cce4090ce767475e4b36af3797eb&oe=61BF0C44',
+    },
+    {
+        id: 2,
+        name: 'Aymeric Larvet',
+        job: 'IoT Engineer',
+        description: 'I know how to comment code in arduino editor.',
+        avatar:
+            'https://cdn.discordapp.com/attachments/684833653242593320/911666122137427998/camphoto_1804928587.JPG',
+    },
+    {
+        id: 3,
+        name: 'Marion Mota',
+        job: 'IA Expert',
+        description: 'Blop ! I take pleasure to develop a fight club with IA.',
+        avatar:
+            'https://scontent-cdg2-1.xx.fbcdn.net/v/t31.18172-8/19956971_1469953669749536_9141682154815299786_o.jpg?_nc_cat=102&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=5EHs9Vw8alsAX91kEAh&_nc_ht=scontent-cdg2-1.xx&oh=e74560bb693839968cbd318aa4873996&oe=61C0ADFA',
+    },
+    {
+        id: 4,
+        name: 'MyOnlyFun',
+        job: 'Professional website',
+        description: 'Just for fun. I promise it\'s not an adult website...',
+        avatar:
+            'https://cdn.discordapp.com/attachments/909802705403392081/909804741314379786/51sdoOtxfKL.png',
+    },
+];
